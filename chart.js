42611819413165ac754e07b0ec37226f76a794e1
James Hong <james.hong@cs.stanford.edu>
2019-10-31 03:25:52 -0700

    Improvements all round

--- chart.js
+++ chart.js
@@ -1,18 +1,32 @@
-const VIDEO_PATH = 'videos.json'
-const FACE_PATH_PREFIX = 'faces/'
+const VIDEO_PATH = 'data/videos.json'
+const FACE_PATH_PREFIX = 'data/faces/'
 
-/* In case we ever want it
- * function timeToMinutes (time) {
-	//convert time to mintues: hhmmss
-	var total_min = 0;
-	for (var i = 0; i < time.length; i+=2) {
-		let time_elt = time.slice(i, i+2);
-		if (i == 0) total_min+=parseInt(time_elt)/60
-		else if (i == 2) total_min+=parseInt(time_elt)
-		else total_min+=parseInt(time_elt)*60
-	}
-	return total_min;
-}*/
+const BTN_EXPANDED = '&#9652;';
+const BTN_HIDDEN = '&#9662;';
+
+const DEFAULT_COLORS = [
+  '#4E79A7', '#F28E2B', '#E15759', '#76B7B2', '#59A14F',
+  '#EDC948', '#B07AA1', '#FF9DA7', '#9C755F', '#BAB0AC'
+];
+
+
+function formatName(s) {
+  return s.split('-').map(t => t.slice(0, 1).toUpperCase() + t.slice(1)).join(' ');
+}
+
+
+function getColor(i) {
+  return DEFAULT_COLORS[i % DEFAULT_COLORS.length];
+}
+
+
+function convertHex(hex, a){
+    hex = hex.replace('#', '');
+    r = parseInt(hex.substring(0,2), 16);
+    g = parseInt(hex.substring(2,4), 16);
+    b = parseInt(hex.substring(4,6), 16);
+    return `rgba(${r},${g},${b},${a})`;
+}
 
 
 function toDecimal(n, k) {
@@ -20,6 +34,11 @@ function toDecimal(n, k) {
     k = 2;
   }
   return n.toLocaleString(undefined, {maximumFractionDigits: k});
+}
+
+
+function reformatDate(s) {
+  return `${s.slice(4, 6)}-${s.slice(6)}-${s.slice(0, 4)}`;
 }
 
 
@@ -40,7 +59,7 @@ function parseVideoName(video_name, width, height, fps) {
     name: video_name,
     channel: channel,
     show: show,
-    date: date, // YYYYMMDD
+    date: reformatDate(date), // YYYYMMDD FIXME: this is UTC time
     time: time, // hhmmss
     width: width,
     height: height,
@@ -77,7 +96,7 @@ function init(person_name, callback) {
 function mapKVToJQueryElements(key, value) {
   return $('<span>').addClass('kv-span').append(
     $('<span>').addClass('key').text(key),
-    $('<span>').addClass('value').text(value));
+    value ? $('<span>').addClass('value').text(value) : null);
 }
 
 
@@ -87,7 +106,7 @@ $.get('frameserver.txt', function(data) {
   FRAME_SERVER_ENDPOINT = $.trim(data);
 });
 
-const VIDEO_WIDTH = 320;
+const VIDEO_WIDTH = 240;
 
 
 function mapFaceToJQueryElements(face) {
@@ -104,10 +123,10 @@ function mapFaceToJQueryElements(face) {
       src: `${ARCHIVE_ENDPOINT}/${face.video.name}/${face.video.name}.mp4?start=${t0}&end=${t1}&exact=1&ignore=x.mp4`
     }).on('loadeddata', resetPlayTime).on('pause', resetPlayTime),
     $('<div>').css({
-      'max-width': VIDEO_WIDTH, 'font-size': 'small'
+      'max-width': VIDEO_WIDTH, 'font-size': 'x-small'
     }).append(
-      $('<span>').text(`face id: ${face.id}, time: ${Math.floor(time)}s`),
-      $('<span>').text(face.video.name)
+      $('<span>').css('overflow-x', 'hidden').text(face.video.name),
+      $('<span>').text(`timestamp: ${Math.floor(time)}s, face id: ${face.id}`)
     ),
   );
   // FIXME: archive player is imprecise with previews, so we have to use
@@ -126,89 +145,12 @@ function sortEntriesByValueListLen(a, b) {
 }
 
 
-// function computeProportions(faces, n_clusters, k_samples_per_cluster) {
-//  	//Goal: to get proportion of clusters per slice
-// 	slice_section = 'channel'; //CHANGE FROM STATIC
-// 	let channels = ['CNN', 'MSNBC', 'FOXNEWS'];
-// 	if (slice_section == 'channel') {
-// 		let cluster_tracker = {'CNN':{}, 'FOXNEWS':{}, 'MSNBC':{}};
-// 		for (face in faces) {
-// 			let curr_channel = faces[face].video.channel;
-// 			var curr_cluster = faces[face].cluster_id;
-// 			if (curr_cluster in cluster_tracker[curr_channel]) {
-// 				cluster_tracker[curr_channel][curr_cluster]+=1;
-// 			} else {
-// 				cluster_tracker[curr_channel][curr_cluster]=1;
-// 			}
-// 			if ('minutes' in cluster_tracker[curr_channel]) {
-// 				cluster_tracker[curr_channel]['minutes'] += 3;
-// 			} else {
-// 				cluster_tracker[curr_channel]['minutes'] = 3;
-// 			}
-// 		}
-// 		console.log(cluster_tracker);
-// 	}
-//
-// }
-
-
 function facesToSeconds(faces) {
   return faces.length * 3;
 }
 
 
-function mapSliceToJQueryElements(
-  faces, n_clusters, k_samples_per_cluster, n_faces_in_all_slices, slice_by
-) {
-  let n_faces_in_slice = faces.length;
-  let faces_by_cluster = {};
-  faces.forEach(f => {
-    let cluster_id = f.cluster_id;
-    if (!faces_by_cluster.hasOwnProperty(cluster_id)) {
-      faces_by_cluster[cluster_id] = [f];
-    } else {
-      faces_by_cluster[cluster_id].push(f);
-    }
-  });
-
-  let faces_by_cluster_arr = Object.entries(
-    faces_by_cluster
-  ).sort(sortEntriesByValueListLen);
-
-  return faces_by_cluster_arr.slice(0, n_clusters).map(
-    ([cluster_id, cluster_faces]) => {
-      let sampled_faces = _.sampleSize(cluster_faces, k_samples_per_cluster);
-      let cluster_seconds = facesToSeconds(cluster_faces);
-      return $('<div>').addClass('cluster-div').append(
-        $('<h3>').text(`cluster ${cluster_id}`),
-        $('<div>').append(
-          mapKVToJQueryElements(
-            'Screen time',
-            `${toDecimal(cluster_seconds)} m`
-          ),
-          mapKVToJQueryElements(
-            `Percent of ${slice_by}`,
-            `${toDecimal(cluster_faces.length / n_faces_in_slice * 100)} %`
-          ),
-          mapKVToJQueryElements(
-            'Percent of total',
-            `${toDecimal(cluster_faces.length / n_faces_in_all_slices * 100)} %`
-          )
-        ),
-        $('<div>').addClass('video-div').append(...sampled_faces.map(mapFaceToJQueryElements))
-      );
-    }
-  );
-}
-
-
-// TODO: make this an argument that can be set by a html input
-const N_SLICES_TO_SHOW = 5;
-const N_CLUSTERS_TO_SHOW = 5;
-const N_VIDEOS_PER_CLUSTER = 6;
-
-
-function getSliceByReducer(video_property) {
+function getSliceByVideoPropertyReducer(video_property) {
   return (acc, face) => {
     let key = face.video[video_property];
     if (acc.hasOwnProperty(key)) {
@@ -221,46 +163,190 @@ function getSliceByReducer(video_property) {
 }
 
 
-function render(div_id, faces, slice_by) {
-  $(div_id).empty();
+function sliceByClusterIdReducer(acc, face) {
+  let k = `Cluster ${face.cluster_id}`;
+  if (acc.hasOwnProperty(k)) {
+    acc[k].push(face);
+  } else {
+    acc[k] = [face];
+  }
+  return acc;
+}
 
-  // Do the slicing (split into groups by slice_by)
-  var slices;
-  if (slice_by == 'all') {
-    slices = {'all': faces};
+
+function sliceFaceList(faces, slice_by) {
+  if (slice_by == '') {
+    slices = {'': faces};
+  } else if (slice_by == 'cluster') {
+    slices = faces.reduce(sliceByClusterIdReducer, {});
   } else if (slice_by == 'channel') {
-    slices = faces.reduce(getSliceByReducer('channel'), {});
+    slices = faces.reduce(getSliceByVideoPropertyReducer('channel'), {});
   } else if (slice_by == 'show') {
-    slices = faces.reduce(getSliceByReducer('show'), {});
+    slices = faces.reduce(getSliceByVideoPropertyReducer('show'), {});
+  } else if (slice_by == 'date') {
+    slices = faces.reduce(getSliceByVideoPropertyReducer('date'), {});
+  }
+  return slices;
+}
+
+
+function mapL1SliceToJQueryElements(
+  l1_slice_faces, slice_by_l2, n_l2_slices,
+  n_faces_in_total, l1_slice_name
+) {
+  let n_faces_in_l1_slice = l1_slice_faces.length;
+  let l2_slices = sliceFaceList(l1_slice_faces, slice_by_l2);
+  let l2_slices_arr = Object.entries(l2_slices).sort(
+    sortEntriesByValueListLen
+  );
+
+  function renderL2Slice(l2_slice) {
+    let [l2_slice_name, l2_slice_faces] = l2_slice;
+    let l2_slice_seconds = facesToSeconds(l2_slice_faces);
+
+    let samplesPerRow = function() {
+      // FIXME: this is hacky and relies on window width and assumptions
+      return Math.floor($(window).width() * 0.9 / VIDEO_WIDTH);
+    }
+
+    let sampleAndRenderVideos = function() {
+      let k = samplesPerRow();
+      let sampled_faces = _.sampleSize(l2_slice_faces, k);
+      return sampled_faces.map(mapFaceToJQueryElements);
+    };
+
+    return $('<div>').addClass('l2-slice-div').append(
+      $('<div>').addClass('kv-div').append(
+        ...(
+          slice_by_l2 ? [
+            mapKVToJQueryElements(l2_slice_name),
+            mapKVToJQueryElements(
+              'Screen time',
+              `${toDecimal(l2_slice_seconds / 60)} min`
+            ),
+            mapKVToJQueryElements(
+              `Percent of "${l1_slice_name}"`,
+              `${toDecimal(l2_slice_faces.length / n_faces_in_l1_slice * 100)} %`
+            ),
+            mapKVToJQueryElements(
+              'Percent of total',
+              `${toDecimal(l2_slice_faces.length / n_faces_in_total * 100)} %`
+            ),
+            $('<button>').addClass('btn btn-secondary btn-sm toggle-l2-slice-btn').attr('type', 'button').html(BTN_EXPANDED).click(
+              function() {
+                let l2_div_elems = $(this).closest('.l2-slice-div').find(
+                  '.video-div, .more-videos-btn, .fewer-videos-btn'
+                );
+                if (l2_div_elems.find(':visible').length) {
+                  l2_div_elems.hide();
+                  $(this).html(BTN_HIDDEN);
+                } else {
+                  l2_div_elems.show();
+                  $(this).html(BTN_EXPANDED);
+                }
+              }
+            )
+          ] : []
+        ),
+        $('<button>').addClass('btn btn-secondary btn-sm more-videos-btn').attr('type', 'button').text('more examples').click(
+          function() {
+            $(this).closest('.l2-slice-div').find('.video-div').append(
+              ...sampleAndRenderVideos()
+            );
+          }
+        ),
+        $('<button>').addClass('btn btn-secondary btn-sm fewer-videos-btn').attr('type', 'button').text('fewer examples').click(
+          function() {
+            let k_to_keep = samplesPerRow();
+            $(this).closest('.l2-slice-div').find('.video-div').find('.vblock').each(
+              function(i) {
+                if (i >= k_to_keep) {
+                  $(this).remove()
+                };
+              }
+            );
+          }
+        ),
+      ),
+      $('<div>').addClass('video-div').append(...sampleAndRenderVideos())
+    );
   }
 
-  let n_faces_in_all_slices = faces.length;
+  let result = l2_slices_arr.slice(0, n_l2_slices).map(renderL2Slice);
+  if (l2_slices_arr.length > n_l2_slices) {
+    let residual_slice_faces = l2_slices_arr.slice(n_l2_slices).flatMap(
+      ([l2_slice_name, l2_slice_faces]) => l2_slice_faces
+    );
+    let residual_slice_name = `Residual (${l2_slices_arr.length - n_l2_slices} ${slice_by_l2}s)`;
+    result.push(renderL2Slice([residual_slice_name, residual_slice_faces]));
+  }
+  return result;
+}
+
+
+function render(div_id, faces, slice_by_l1, slice_by_l2, n_l1_slices, n_l2_slices, start_expanded) {
+  $(div_id).empty();
+  let n_faces_in_total = faces.length;
+
+  // Do the slicing (split into groups by slice_by)
+  let l1_slices = sliceFaceList(faces, slice_by_l1);
+  let l1_slices_arr = Object.entries(l1_slices).sort(
+    sortEntriesByValueListLen
+  );
+
+  function renderL1Slice(l1_slice, i) {
+    let [l1_slice_name, l1_slice_faces] = l1_slice;
+    let l1_slice_seconds = facesToSeconds(l1_slice_faces);
+    return $('<div>').addClass('l1-slice-div').css(
+      'background-color', convertHex(getColor(i), 0.5)
+    ).append(
+      $('<div>').addClass('kv-div').append(
+        mapKVToJQueryElements(l1_slice_name),
+        mapKVToJQueryElements(
+          'Screen time',
+          `${toDecimal(l1_slice_seconds / 60)} min`
+        ),
+        mapKVToJQueryElements(
+          `Percent of total`,
+          `${toDecimal(l1_slice_faces.length / n_faces_in_total * 100)} %`
+        ),
+        $('<button>').addClass('btn btn-secondary btn-sm toggle-l1-slice-btn').html(BTN_EXPANDED).attr('type', 'button').click(
+          function() {
+            let l2_divs = $(this).closest('.l1-slice-div').find('.l2-slice-div');
+            if (l2_divs.find(':visible').length) {
+              l2_divs.hide();
+              $(this).html(BTN_HIDDEN);
+            } else {
+              l2_divs.show();
+              $(this).html(BTN_EXPANDED);
+            }
+          }
+        ),
+      ),
+      mapL1SliceToJQueryElements(
+        l1_slice_faces, slice_by_l2, n_l2_slices,
+        n_faces_in_total, l1_slice_name)
+    );
+  };
+
+  function renderResidual() {
+    let residual_slice_faces = l1_slices_arr.slice(n_l1_slices).flatMap(
+      ([l1_slice_name, l1_slice_faces]) => l1_slice_faces
+    );
+    let residual_slice_name = `Residual (${l1_slices_arr.length - n_l1_slices} ${slice_by_l1}s)`;
+    return renderL1Slice([residual_slice_name, residual_slice_faces], n_l1_slices);
+  }
 
   // Use jquery to write html with videos
   $(div_id).append(
     // Convert slices to JQuery objects for HTML
-    ...Object.entries(slices).sort(
-      sortEntriesByValueListLen
-    ).slice(0, N_SLICES_TO_SHOW).map(
-      ([slice_name, slice_faces]) => {
-        let slice_seconds = facesToSeconds(slice_faces);
-        return $('<div>').addClass('slice-div').append(
-          $('<h2>').addClass('title').text(slice_name),
-          $('<div>').append(
-            mapKVToJQueryElements(
-              'Screen time',
-              `${toDecimal(slice_seconds)} m`
-            ),
-            mapKVToJQueryElements(
-              `Percent of total`,
-              `${toDecimal(slice_faces.length / n_faces_in_all_slices * 100)} %`
-            )
-          ),
-          mapSliceToJQueryElements(
-            slice_faces, N_CLUSTERS_TO_SHOW, N_VIDEOS_PER_CLUSTER,
-            n_faces_in_all_slices, slice_by)
-        );
-      }
-    )
+    ...l1_slices_arr.slice(0, n_l1_slices).map(renderL1Slice),
+    l1_slices_arr.length <= n_l1_slices ? null : renderResidual()
   );
+
+  if (!start_expanded) {
+    $('.toggle-l1-slice-btn').each(function() {
+      $(this).click(); // everything is expanded by default
+    });
+  }
 }
